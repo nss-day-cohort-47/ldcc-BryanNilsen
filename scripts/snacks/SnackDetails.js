92e36883d881b55d46b26a6a88ce2a8fd10bf81c
BryanNilsen <bryannilsen@gmail.com>
2021-03-26 10:45:05 -0500

    details updated with resource names instead of ids

--- scripts/snacks/SnackDetails.js
+++ scripts/snacks/SnackDetails.js
@@ -1,24 +1,24 @@
 export const SnackDetails = (snackObject) => {
-	return `
+    return `
 	<div class="col">
 		<div class="card shadow-sm" >
             <img class="bd-placeholder-img card-img-top"  style="max-width: 540px;" aria-label="Placeholder:${snackObject.name}" preserveAspectRatio="xMidYMid slice" focusable="false" src="${snackObject.snackImg}" alt="${snackObject.name}">
             <div class="card-body">
 				<h5 color="primary">${snackObject.name}</h5>
               	<p class="card-text">${snackObject.description}</p>
-				 
+
 				  <div class="container">
 					<div class="row row-cols-2">
-						<div class="col col-details">Type: ${snackObject.typeId}</div>
-						<div class="col col-details">Shape: ${snackObject.shapeId}</div>
-						<div class="col col-details">Flavor: ${snackObject.inFlavorId}</div>
-						<div class="col col-details">Season: ${snackObject.seasonId}</div>
+						<div class="col col-details">Type: ${snackObject.type.name}</div>
+						<div class="col col-details">Shape: ${snackObject.shape.name}</div>
+						<div class="col col-details">Flavor: ${snackObject.inFlavor.name}</div>
+						<div class="col col-details">Season: ${snackObject.season.name}</div>
 					</div>
 					<div class="row row-cols-1">
 						<div class="col col-details">${snackObject.toppings}</div>
 					</div>
 				</div>
-			  	
+
 				<div class="d-flex justify-content-between align-items-center">
 					<div class="btn-group">
 					<button type="button" class="btn btn-sm btn-outline-secondary" id="editcake__${snackObject.id}" disabled>Edit</button>
